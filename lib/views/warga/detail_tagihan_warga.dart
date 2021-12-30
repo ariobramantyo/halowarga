3ed00f1b95c735e0eb7615b4b16514fd5e695238
ariobramantyo <i.ario.bramantyo@gmail.com>
2021-12-30 09:43:14 +0700

    create view montly payment status for member

--- lib/views/warga/detail_tagihan_warga.dart
+++ lib/views/warga/detail_tagihan_warga.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:halowarga/const/colors.dart';
+import 'package:halowarga/controller/user_controller.dart';
+import 'package:halowarga/controller/year_controller.dart';
+import 'package:halowarga/model/financeReport.dart';
 import 'package:intl/intl.dart';
 
 class DetailTagihanWarga extends StatefulWidget {
@@ -16,6 +20,90 @@ class DetailTagihanWarga extends StatefulWidget {
 class _DetailTagihanWargaState extends State<DetailTagihanWarga>
     with TickerProviderStateMixin {
   late TabController _tabController;
+
+  final _yearController = Get.put(YearController());
+  final _userController = Get.find<UserController>();
+
+  final List<String> _listMonth = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ];
+
+  bool _isDataExist(String value, List<FinanceReport> list) {
+    var data = list.where((row) {
+      return row.month == value;
+    });
+    if (data.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool _checkCurrentMonth(String month) {
+    return DateFormat('MMM').format(DateTime.now()) == month;
+  }
+
+  Widget monthCard(String month, List<FinanceReport> list) {
+    return Container(
+      height: 100,
+      width: 80,
+      decoration: BoxDecoration(
+          color: _isDataExist(month, list) ? AppColor.mainColor : AppColor.red,
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black12,
+                blurRadius: 5,
+                spreadRadius: 1,
+                offset: Offset(2, 2))
+          ]),
+      child: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                month,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: AppColor.white,
+                    fontSize: 17,
+                    fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 5),
+              Icon(
+                _isDataExist(month, list)
+                    ? Icons.check_circle
+                    : Icons.remove_circle,
+                color: AppColor.white,
+              )
+            ],
+          ),
+          Container(
+            height: 10,
+            margin: EdgeInsets.symmetric(vertical: 10),
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: _checkCurrentMonth(month)
+                    ? AppColor.white
+                    : Colors.transparent),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   void initState() {
@@ -118,14 +206,163 @@ class _DetailTagihanWargaState extends State<DetailTagihanWarga>
                 ],
               ),
             ),
+            Container(
+              height: 60,
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              color: AppColor.white,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Obx(() => Text(
+                        'Tagihan ${_yearController.year.value}',
+                        style: TextStyle(
+                            fontSize: 17, fontWeight: FontWeight.w500),
+                      )),
+                  Row(
+                    children: [
+                      IconButton(
+                          onPressed: () => _yearController.decreaseYear(),
+                          padding: EdgeInsets.zero,
+                          icon: Icon(Icons.chevron_left)),
+                      Obx(
+                        () => IconButton(
+                            onPressed: () => _yearController.increaseYear(),
+                            padding: EdgeInsets.zero,
+                            icon: Icon(
+                              Icons.chevron_right,
+                              color: _yearController.isMaxYear()
+                                  ? AppColor.secondaryText
+                                  : AppColor.black,
+                            )),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
             Expanded(
                 child: Container(
               child: TabBarView(
                 controller: _tabController,
                 children: [
-                  Icon(Icons.shield),
-                  Icon(Icons.cleaning_services),
-                  Icon(Icons.show_chart)
+                  // tab keamanan
+                  Obx(
+                    () => StreamBuilder<QuerySnapshot>(
+                      stream: FirebaseFirestore.instance
+                          .collection('user')
+                          .doc(_userController.loggedUser.value.uid)
+                          .collection('monthlyPayment')
+                          .where('typeIncome', isEqualTo: 'Keamanan')
+                          .where('year',
+                              isEqualTo: _yearController.year.toString())
+                          .snapshots(),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          var _data1 = snapshot.data!.docs
+                              .map<FinanceReport>((e) =>
+                                  FinanceReport.fromSnapshot(e
+                                      as QueryDocumentSnapshot<
+                                          Map<String, dynamic>>))
+                              .toList();
+                          return ListView(
+                            physics: BouncingScrollPhysics(),
+                            children: [
+                              SizedBox(height: 20),
+                              Wrap(
+                                  spacing: 20,
+                                  runSpacing: 20,
+                                  alignment: WrapAlignment.center,
+                                  children: _listMonth
+                                      .map<Widget>(
+                                          (month) => monthCard(month, _data1))
+                                      .toList()),
+                              SizedBox(height: 20),
+                            ],
+                          );
+                        }
+                        return Center(child: CircularProgressIndicator());
+                      },
+                    ),
+                  ),
+                  // tab kebersihan
+                  Obx(
+                    () => StreamBuilder<QuerySnapshot>(
+                      stream: FirebaseFirestore.instance
+                          .collection('user')
+                          .doc(_userController.loggedUser.value.uid)
+                          .collection('monthlyPayment')
+                          .where('typeIncome', isEqualTo: 'Kebersihan')
+                          .where('year',
+                              isEqualTo: _yearController.year.toString())
+                          .snapshots(),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          var _data2 = snapshot.data!.docs
+                              .map<FinanceReport>((e) =>
+                                  FinanceReport.fromSnapshot(e
+                                      as QueryDocumentSnapshot<
+                                          Map<String, dynamic>>))
+                              .toList();
+                          return ListView(
+                            physics: BouncingScrollPhysics(),
+                            children: [
+                              SizedBox(height: 20),
+                              Wrap(
+                                  spacing: 20,
+                                  runSpacing: 20,
+                                  alignment: WrapAlignment.center,
+                                  children: _listMonth
+                                      .map<Widget>(
+                                          (month) => monthCard(month, _data2))
+                                      .toList()),
+                              SizedBox(height: 20),
+                            ],
+                          );
+                        }
+                        return Center(child: CircularProgressIndicator());
+                      },
+                    ),
+                  ),
+                  // tab iuran bulanab
+                  Obx(
+                    () => StreamBuilder<QuerySnapshot>(
+                      stream: FirebaseFirestore.instance
+                          .collection('user')
+                          .doc(_userController.loggedUser.value.uid)
+                          .collection('monthlyPayment')
+                          .where('typeIncome', isEqualTo: 'Bayar Iuran')
+                          .where('year',
+                              isEqualTo: _yearController.year.toString())
+                          .snapshots(),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          var _data3 = snapshot.data!.docs
+                              .map<FinanceReport>((e) =>
+                                  FinanceReport.fromSnapshot(e
+                                      as QueryDocumentSnapshot<
+                                          Map<String, dynamic>>))
+                              .toList();
+                          return ListView(
+                            physics: BouncingScrollPhysics(),
+                            children: [
+                              SizedBox(height: 20),
+                              Wrap(
+                                  spacing: 20,
+                                  runSpacing: 20,
+                                  alignment: WrapAlignment.center,
+                                  children: _listMonth
+                                      .map<Widget>(
+                                          (month) => monthCard(month, _data3))
+                                      .toList()),
+                              SizedBox(height: 20),
+                            ],
+                          );
+                        }
+                        return Center(child: CircularProgressIndicator());
+                      },
+                    ),
+                  ),
                 ],
               ),
             ))
